34f9eb6ad9917dcc990bd22265b156143146d82a
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2024-02-10 23:11:11 +0200

    commit-3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,38 @@
 // Темная тема
 document.getElementById('themeToggle').addEventListener('click', function() {
-    let body = document.body;
-    let navbar = document.querySelector('.navbar');
-    let cardBodies = document.querySelectorAll('.card-body');
-    let imgTheme = document.querySelectorAll('.img-theme');
-    let themeIcon = document.getElementById('themeIcon');
-    body.classList.toggle('dark-theme');
-    navbar.classList.toggle('dark-theme-navbar');
-    imgTheme.forEach(img => img.classList.toggle('img-theme-dark'));
-    cardBodies.forEach(cardBody => cardBody.classList.toggle('card-body-theme'));
-    if (body.classList.contains('dark-theme')) {
-        themeIcon.classList.remove('fa-moon');
-        themeIcon.classList.add('fa-sun');
-    } else {
-        themeIcon.classList.remove('fa-sun');
-        themeIcon.classList.add('fa-moon');
-    }
+  let body = document.body;
+  let navbar = document.querySelector('.navbar');
+  let header = document.querySelector('header');
+  let navLinks = document.querySelectorAll('.nav-link');
+  let navbarBrandIcon = document.querySelector('.navbar-brand i');
+  let themeToggle = document.getElementById('themeToggle'); // Добавьте эту строку
+  let cardBodies = document.querySelectorAll('.card-body');
+  let imgTheme = document.querySelectorAll('.img-theme');
+  let themeIcon = document.getElementById('themeIcon');
+  body.classList.toggle('dark-theme');
+  navbar.classList.toggle('dark-theme-navbar');
+  header.classList.toggle('dark-theme-header');
+  navLinks.forEach(navLink => navLink.classList.toggle('dark-theme-text'));
+  navbarBrandIcon.classList.toggle('dark-theme-icon');
+  themeToggle.classList.toggle('dark-theme-button'); // И эту строку
+  imgTheme.forEach(img => img.classList.toggle('img-theme-dark'));
+  cardBodies.forEach(cardBody => cardBody.classList.toggle('card-body-theme'));
+  if (body.classList.contains('dark-theme')) {
+      themeIcon.classList.remove('fa-moon');
+      themeIcon.classList.add('fa-sun');
+  } else {
+      themeIcon.classList.remove('fa-sun');
+      themeIcon.classList.add('fa-moon');
+  }
 });
 
+
+// Цвет фона
+document.getElementById('colorPicker').addEventListener('input', function() {
+  document.body.style.backgroundColor = this.value;
+});
+
+// Vue.js
 const app = Vue.createApp({
   data() {
     return {
