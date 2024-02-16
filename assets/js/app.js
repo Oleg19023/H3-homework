c230c63a1a577016b9ad018ebf293c136fc8b48a
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2024-02-16 17:17:31 +0200

    commit-5.5fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ document.getElementById('themeToggle').addEventListener('click', function() {
   let header = document.querySelector('header');
   let navLinks = document.querySelectorAll('.nav-link');
   let navbarBrandIcon = document.querySelector('.navbar-brand i');
-  let themeToggle = document.getElementById('themeToggle'); // Добавьте эту строку
+  let themeToggle = document.getElementById('themeToggle');
   let cardBodies = document.querySelectorAll('.card-body');
   let imgTheme = document.querySelectorAll('.img-theme');
   let themeIcon = document.getElementById('themeIcon');
@@ -14,7 +14,7 @@ document.getElementById('themeToggle').addEventListener('click', function() {
   header.classList.toggle('dark-theme-header');
   navLinks.forEach(navLink => navLink.classList.toggle('dark-theme-text'));
   navbarBrandIcon.classList.toggle('dark-theme-icon');
-  themeToggle.classList.toggle('dark-theme-button'); // И эту строку
+  themeToggle.classList.toggle('dark-theme-button');
   imgTheme.forEach(img => img.classList.toggle('img-theme-dark'));
   cardBodies.forEach(cardBody => cardBody.classList.toggle('card-body-theme'));
   if (body.classList.contains('dark-theme')) {
